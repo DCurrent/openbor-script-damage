0b03a3e6af1637f3e77a913ea2b539bd7f0b9c6e
Caskey, Damon V <dc@caskeys.com>
2018-11-13 12:40:45 -0500

    Add entity target access / mutators.

--- damage.c
+++ damage.c
@@ -1,3 +1,26 @@
 #include "data/scripts/dc_damage/config.h"
 
 #import "data/scripts/dc_damage/config.c"
+
+// Base entity functions will act on.
+void dc_damage_get_entity()
+{
+	int instance;
+	void result;
+
+	result = getlocalvar(instance + DC_DAMAGE_VAR_KEY_ENT);
+
+	if (typeof(result) != openborconstant("VT_PTR"))
+	{
+		result = DC_DAMAGE_DEFAULT_ENT;
+	}
+
+	return result;
+}
+
+void dc_damage_set_entity(void value)
+{
+	int instance;
+
+	setlocalvar(instance + DC_DAMAGE_VAR_KEY_ENT, value);
+}
