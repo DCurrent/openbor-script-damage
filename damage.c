87a514b4e53de5f211c991f7ab69d6f934fd9bfe
Caskey, Damon V <dc@caskeys.com>
2018-11-13 14:14:21 -0500

    Add other access and mutate.

--- damage.c
+++ damage.c
@@ -24,3 +24,26 @@ void dc_damage_set_entity(void value)
 
 	setlocalvar(instance + DC_DAMAGE_VAR_KEY_ENT, value);
 }
+
+// Base entity functions will act on.
+void dc_damage_get_other()
+{
+	int instance;
+	void result;
+
+	result = getlocalvar(instance + DC_DAMAGE_VAR_KEY_OTHER);
+
+	if (typeof(result) != openborconstant("VT_PTR"))
+	{
+		result = DC_DAMAGE_DEFAULT_OTHER;
+	}
+
+	return result;
+}
+
+void dc_damage_set_other(void value)
+{
+	int instance;
+
+	setlocalvar(instance + DC_DAMAGE_VAR_KEY_OTHER, value);
+}
