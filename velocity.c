b69317431825d19a50de839bf2e3c6f3f4817afc
Damon V. Caskey <dc@caskeys.com>
2019-10-20 02:11:58 -0400

    Update velocity.c
    Remove head inserts.

--- velocity.c
+++ velocity.c
@@ -1,10 +1,6 @@
 #include "data/scripts/dc_damage/config.h"
-<<<<<<< HEAD
 #import "data/scripts/dc_damage/instance.c"
 #import "data/scripts/dc_damage/config.c"
-=======
->>>>>>> 14e84180146fe947a77689ebce04bf4b97cdcfcc
-
 #import "data/scripts/dc_damage/instance.c"
 
 // Knockdown velocity to apply, X axis.
