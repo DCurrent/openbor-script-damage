336c70f0dbbbbb7a8468a4ed289a14281c5e0c26
Damon V. Caskey <dc@caskeys.com>
2019-10-20 02:13:09 -0400

    Update damage.c
    Remove head inserts.

--- damage.c
+++ damage.c
@@ -1,11 +1,8 @@
 #include "data/scripts/dc_damage/config.h"
-
-<<<<<<< HEAD
 #import "data/scripts/dc_damage/config.c"
 #import "data/scripts/dc_damage/instance.c"
 #import "data/scripts/dc_damage/direction.c"
 #import "data/scripts/dc_damage/lethality.c"
-=======
 #import "data/scripts/dc_damage/damage_force.c"
 #import "data/scripts/dc_damage/damage_land.c"
 #import "data/scripts/dc_damage/damage_type.c"
@@ -14,7 +11,6 @@
 #import "data/scripts/dc_damage/entity.c"
 #import "data/scripts/dc_damage/instance.c"
 #import "data/scripts/dc_damage/projectile.c"
->>>>>>> 14e84180146fe947a77689ebce04bf4b97cdcfcc
 #import "data/scripts/dc_damage/toss.c"
 #import "data/scripts/dc_damage/velocity.c"
 
